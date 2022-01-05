c524429c005b3c3103c1ab14e0af28102a314e03
EC2 Default User <ec2-user@ip-172-31-85-123.ec2.internal>
2022-01-05 05:52:24 +0000

    [Finish] 3章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '09878d19bca5046ca5501f1eb98a7f0a8ba0d2e423aa40d9a84af56ead496cbbefea327a5c88969c250c55a37101f5ca2c5f869da9e519c90bf90f440241d842'
